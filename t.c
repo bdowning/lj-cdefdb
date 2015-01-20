fcdd12baec6355f27308447af1d8c077607eeaf8
Brian Downing <bdowning@lavos.net>
2015-01-19 21:15:15 -0600

    Add C file with test cases

---
+++ t.c
@@ -0,0 +1,44 @@
+struct {
+    int f_unnamed;
+};
+
+enum { N = 4 };
+
+struct sz { int foo; int bar; };
+
+typedef struct {
+    int f_boo;
+} ***foo[N*2+sizeof(struct sz)][N*N], foonp;
+
+typedef struct bar {
+    int f_bar;
+} ***const bar[N*2+sizeof(struct sz)][N*N*N], foon;
+
+typedef struct baz baz;
+struct baz {
+    int f_baz;
+};
+
+struct quux {
+    int f_quux;
+};
+typedef struct quux quux;
+
+struct b;
+
+struct a {
+    void (*call)(struct b *foo);
+};
+
+struct b {
+    int in_b;
+};
+
+struct rec {
+    struct rec *rec;
+};
+
+union u {
+    int a;
+    char *b;
+};
